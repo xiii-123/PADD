b1f6cb5fbc6992d999984e2d93d5760f2b220b9f
wyh <1058827330@qq.com>
2025-04-02 15:33:20 +0800

    remove build

--- build/lib/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wyh/POS/PADD/PADD01/lib/googletest/googletest
-# Build directory: /home/wyh/POS/PADD/PADD01/build/lib/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
